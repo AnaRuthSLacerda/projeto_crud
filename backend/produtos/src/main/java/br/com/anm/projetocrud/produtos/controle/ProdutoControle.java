22255ed2ed18efcc512af09ec53fb63ae981c625
RuthLacerda <141785762+AnaRuthSLacerda@users.noreply.github.com>
2025-03-18 11:45:33 -0300

    alteração metodo  cadastrar, criação rota para remover e CORS resolvido!

--- backend/produtos/src/main/java/br/com/anm/projetocrud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/anm/projetocrud/produtos/controle/ProdutoControle.java
@@ -1,7 +1,10 @@
 package br.com.anm.projetocrud.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.anm.projetocrud.produtos.modelo.ProdutoModelo;
@@ -11,7 +14,12 @@ import br.com.anm.projetocrud.produtos.service.ProdutoService;
 public class ProdutoControle {
 @Autowired
   private ProdutoService ps;
-  
+
+  @PostMapping("/cadastrar")
+  public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+    return ps.cadastrar(pm);
+  }
+
     @GetMapping("/listar")
   public Interabe<ProdutoModelo>listar(){
       return ps.listar();
--- backend/produtos/src/main/java/br/com/anm/projetocrud/produtos/service/ProdutoService.java
+++ backend/produtos/src/main/java/br/com/anm/projetocrud/produtos/service/ProdutoService.java
@@ -1,10 +1,13 @@
 package br.com.anm.projetocrud.produtos.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.anm.projetocrud.produtos.controle.Interabe;
 import br.com.anm.projetocrud.produtos.modelo.ProdutoModelo;
+import br.com.anm.projetocrud.produtos.modelo.RespostaModelo;
 import br.com.anm.projetocrud.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -12,6 +15,9 @@ public class ProdutoService {
 
   @Autowired
   private ProdutoRepositorio pr;
+
+  @Autowired
+  private RespostaModelo rm;
 
   public Iterable<ProdutoModelo> lister() {
     return pr.findAll();
@@ -22,4 +28,16 @@ public class ProdutoService {
     throw new UnsupportedOperationException("Unimplemented method 'listar'");
   }
 
+  // CadastrarProduto
+  public ResponseEntity<?> cadastrar(ProdutoModelo pm) {
+    if (pm.getNome().equals("")) {
+      rm.setResposta("O nome do produto é obrigatório.");
+      return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+    } else if (pm.getMarca().equals("")) {
+      rm.setResposta("O nome da marca do produto é obrigatório.");
+      return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+    } else {
+      return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+    }
+  }
 }
